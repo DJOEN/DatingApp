588278a57b1584c4d2542ccc1a9099fc5da70ad4
Davur <joensen.davur@gmail.com>
2025-12-14 22:06:37 +0100

    Added DB

--- API/Program.cs
+++ API/Program.cs
@@ -6,13 +6,17 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
 builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
 var app = builder.Build();
+app.Logger.LogInformation("Starting app, Please wait!!!!!!!!!!!");
 
 app.MapControllers();
 
 app.Run();
+
